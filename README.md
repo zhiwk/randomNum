a83ae0773773fb1d0c655259a725ec6aa8487de3
zhiwk <1198865462@qq.com>
2021-09-20 17:27:40 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 ## 界面展示
 
-![](https://files.catbox.moe/jesg0t.png)
+![](https://files.catbox.moe/or5zw5.png)
 
 ## 优点
 
@@ -19,4 +19,14 @@
 输入数字长度存在限制
 
 不能生成小数的随机数
+
+## 版本更新
+
+### 0.0.2
+
+- 能够生成随机小数
+
+- 给自己设计了一个logo，准备长期使用，也不怕侵权，嘻嘻
+
+![](./public/logo.png)
 
--- src/components/randomNum.vue
+++ src/components/randomNum.vue
@@ -19,15 +19,41 @@
     </el-row>
   </li>
   <div>
-    <el-radio v-model="radio" label="1">唯一</el-radio>
-    <el-radio v-model="radio" label="2">不唯一</el-radio>
+    <el-radio v-model="littleNum" label="1">整数</el-radio>
+    <el-radio v-model="littleNum" label="2">小数</el-radio>
   </div>
+  <div>
+    <el-row id="buf_hi" v-show="this.littleNum == '1' ? false : true">
+      <el-col :span="5"></el-col>
+      <el-col :span="5">
+        <p>保留小数部分的位数：</p>
+      </el-col>
+      <el-col :span="4">
+        <el-input
+          type="number"
+          v-model="littleLen"
+          placeholder="请输入内容"
+          maxlength="6"
+        ></el-input>
+      </el-col>
+      <!-- <el-col :span="2"></el-col> -->
+      <el-col :span="2"><p>(1~10)</p></el-col>
+      <el-col :span="8"></el-col>
+    </el-row>
+  </div>
+
+  <div id="buf_hi" v-show="this.littleNum == '1' ? true : false">
+    <el-radio v-model="only" label="1">唯一</el-radio>
+    <el-radio v-model="only" label="2">不唯一</el-radio>
+    <!-- <div id='buf_hi'></div> -->
+  </div>
+
   <el-button type="primary" @click="getResult()">生成generate</el-button>
-  <el-button type="primary" @click="getResultz(10)"
-    >生成一个1~10的随机数</el-button
-  >
   <el-button type="primary" @click="getResultz(100)"
-    >生成一个1~100的随机数</el-button
+    >生成一个0~100的随机整数</el-button
+  >
+  <el-button type="primary" @click="getResultl(100)"
+    >生成一个0~100的随机小数</el-button
   >
   <p>{{ result }}</p>
 </template>
@@ -45,10 +71,28 @@ export default defineComponent({
         { str: "最大值", num: ref("") },
       ],
       result: ref(""),
-      radio: "1",
+      littleNum: "1",
+      only: "1",
+
+      littleLen: ref(""),
     };
   },
   methods: {
+    RandomLitList(num, min, max, num_len) {
+      if (min >= max) {
+        ElMessage.error({
+          message: "最小值应当比最大值小",
+          type: "error",
+        });
+        return;
+      }
+      var points = [];
+      var range = max - min;
+      for (var i = 0; i < num; i++) {
+        points.push(Number((min + Math.random() * range).toFixed(num_len)));
+      }
+      return points;
+    },
     RandomList(num, min, max) {
       if (min >= max) {
         ElMessage.error({
@@ -94,8 +138,6 @@ export default defineComponent({
     },
 
     getResult() {
-        // console.log(this.inputs[0].num);
-        // console.log(this.inputs[0].num.indexOf("."));
       if (
         (this.inputs[0].num == "") |
         (this.inputs[1].num == "") |
@@ -106,31 +148,69 @@ export default defineComponent({
           type: "error",
         });
         return;
-      } else if (
-        this.inputs[0].num.indexOf(".") != -1 |
-        this.inputs[1].num.indexOf(".") != -1 |
-        this.inputs[2].num.indexOf(".") != -1
-      ) {
-        ElMessage.error({
-          message: "数值只能为整数",
-          type: "error",
-        });
-        return;
-      } else {
-        if (Number(this.inputs[0].num) <= 0) {
+      }
+      // else if (
+      //   (this.inputs[0].num.indexOf(".") != -1) |
+      //   (this.inputs[1].num.indexOf(".") != -1) |
+      //   (this.inputs[2].num.indexOf(".") != -1)
+      // ) {
+      //   ElMessage.error({
+      //     message: "数值只能为整数",
+      //     type: "error",
+      //   });
+      //   return;
+      // }
+      else {
+        if (Number(this.inputs[0].num) <= 0 | (this.inputs[0].num.indexOf(".") != -1)) {
           ElMessage.error({
             message: "数量必须为正整数",
             type: "error",
           });
           return;
         }
-        if (this.radio == "1") {
+        if (this.littleNum == "2") {
+          if (this.littleLen == "") {
+            ElMessage.error({
+              message: "输入框不能为空",
+              type: "error",
+            });
+            return;
+          } else if (
+            (Number(this.littleLen) > 10) |
+            (Number(this.littleLen) < 1) |
+            (this.littleLen.indexOf(".") != -1)
+          ) {
+            ElMessage.error({
+              message: "小数部分保留位数错误",
+              type: "error",
+            });
+            return;
+          }
+          this.result = this.RandomLitList(
+            Number(this.inputs[0].num),
+            Number(this.inputs[1].num),
+            Number(this.inputs[2].num),
+            Number(this.littleLen)
+          );
+          return;
+        }
+        if (
+          (this.inputs[0].num.indexOf(".") != -1) |
+          (this.inputs[1].num.indexOf(".") != -1) |
+          (this.inputs[2].num.indexOf(".") != -1)
+        ) {
+          ElMessage.error({
+            message: "数值只能为整数",
+            type: "error",
+          });
+          return;
+        }
+        if (this.only == "1") {
           this.result = this.RandomList(
             Number(this.inputs[0].num),
             Number(this.inputs[1].num),
             Number(this.inputs[2].num)
           );
-          //   console.log(this.result[0]);
         } else {
           this.result = this.RandomListN(
             Number(this.inputs[0].num),
@@ -141,7 +221,10 @@ export default defineComponent({
       }
     },
     getResultz(max) {
-      this.result = this.RandomList(Number(1), Number(1), Number(max));
+      this.result = this.RandomList(Number(1), Number(0), Number(max));
+    },
+    getResultl(max) {
+      this.result = this.RandomLitList(Number(1), Number(0), Number(max), 10);
     },
   },
 });
@@ -150,5 +233,8 @@ export default defineComponent({
 <style>
 #numInput {
   list-style: none;
+}
+#buf_hi {
+  height: 53px;
 }
 </style>
